14305f815b1ebcdb679495fa21ac2319758c9c28
jarpi <josep.arpi@gmail.com>
2016-11-06 22:13:18 +0100

    Refactor websocket layer - Added layer to initialize ws - Purged index - Runnable from cloud instance / custom host

--- index.js
+++ index.js
@@ -5,30 +5,12 @@ var express = require('express'),
     nodemon = require('nodemon'),
     winston = require('winston'),
     http = require('http').Server(app),
-    uuid = require('uuid'),
-    WebSocketServer = require('ws').Server;
-    var wss = new WebSocketServer({ server: http }),
+    wss = require('./src/ws.js'),
     port = process.env.port || 8088;
 
-wss.on('connection', function(socket){
-  socket.id = uuid.v4();
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('message', function(msg){
-    console.log('Message received');
-    wss.broadcast(msg, socket.id);
-  });
-});
-
-// TODO: REFACTOR
-wss.broadcast = function(msg, sender) {
-    wss.clients.forEach(function(client){
-        if (client.id !== sender) client.send(msg) & winston.info('Message sent!');
-    });
-}
-
-http.listen(port, function(){
+http.listen(port, function() {
     winston.info('Application server running!');
+    new wss(this, winston, function(err){
+        if (!err) winston.info('Websocket listening!');
+    });
 });
